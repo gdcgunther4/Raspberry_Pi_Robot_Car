735d17c7aa9b5fe6d4dc8a08017609f9b80a838d
gdcgunther4 <61941049+gdcgunther4@users.noreply.github.com>
2020-07-21 09:41:43 -0400

    Update Mk_1.py

--- Mk_1.py
+++ Mk_1.py
@@ -1,11 +1,12 @@
 '''
 This was inspired by the book 'Learn Robotics with Raspberry Pi
 '''
-
+#This sets up the motors
 import cwiid
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
+#Now we connect to the Wii remote
 import cwiid, time
 
 button_delay = 0.1
@@ -24,9 +25,9 @@ print ('Wiimote connection established!\n')
 
 
 time.sleep(3)
-
+#This activates the buttons, acclerometer, and the nunchuck
 wii.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_ACC | cwiid.RPT_NUNCHUK
-
+#Now we set up the motor functions
 def moveR(speedR):
     kit.motor1.throttle = speedR
     kit.motor2.throttle = speedR
@@ -37,22 +38,23 @@ def moveL(speedL):
 
 while True:
     #This section defines the wii values. In the future, I will be using NunchuckStickX and NunchuckStickY to control mecanum wheels. Other properties will probably also be used.
-    
+    #Wiimote buttons
     buttons = wii.state['buttons']
+    #this is the nunchuck buttons
     NunchuckBTN = wii.state['nunchuk']['buttons']
-    #X axis:LeftMax = 25, Middle = 125, RightMax = 225
+    #The joystick X axis
     NunchukStickX = (wii.state['nunchuk']['stick'][cwiid.X] - 127)
-    #Y axis:DownMax = 30, Middle = 125, UpMax = 225
+    #Now for the joystick's Y axis
     NunchukStickY = (wii.state['nunchuk']['stick'][cwiid.Y] - 128)
-    #The nunchuk has an accelerometer that records in a similar manner to the wiimote, but the number range is different
-    #The X range is: 70 if tilted 90 degrees to the left and 175 if tilted 90 degrees to the right
+    #Now for the acclerometer
+    #Nunchuck X axis
     NAccx = (wii.state['nunchuk']['acc'][cwiid.X] - 130)
-    #The Y range is: 70 if tilted 90 degrees down (the buttons pointing down), and 175 if tilted 90 degrees up (buttons pointing up)
+    #Nunchuck Y axis
     NAccy = (wii.state['nunchuk']['acc'][cwiid.Y] - 130) / 50
-    #I still don't understand the z axis completely (on the wiimote and nunchuk), but as far as I can tell it's main change comes from directly pulling up the mote without tilting it
+    #Nunchuck Z axis
     NAccz = (wii.state['nunchuk']['acc'][cwiid.Z] - 130)
     forward = -(wii.state['acc'][cwiid.Y] - 125) /100 * 4
-
+    #This section makes
     if (forward > .96):
         forward  = .96
     if (forward < -.96):
@@ -61,7 +63,7 @@ while True:
         NAccy  = .96
     if (NAccy < -.96):
         NAccy = -.96
-
+    #these sections control motion
     if (buttons & cwiid.BTN_B):
         moveR(forward)
     else:
